56792adf5a07be3472544c216836ac85171d4aa0
Simone <bertoni.simone@gmail.com>
2023-06-18 19:50:58 +0100

    Add comments

--- Pendulum - SMC.py
+++ Pendulum - SMC.py
@@ -10,15 +10,22 @@ project_uuid = "221181d1-4494-4cf8-a58a-61345a7aa14c"
 collimator.set_auth_token(token, project_uuid)
 model = collimator.load_model("Pendulum - Sliding Mode Control")
 
+# Create array of mass values for robustness analysis
 m_V = [0.1, 0.5, 1.2]
+
+# Create array of results
 res_V = []
 
+# Simulate for each mass value and store results in res_V
 for m in m_V:
     sim = collimator.run_simulation(model, parameters = {'m': m, 'W':0.5,'lambda_':1, "tau_d":0.1})
     res = sim.results.to_pandas()
     res_V.append(res)
 
+# Create figure
 plt.figure()
+
+# Plot response for each mass value and setpoint in 1st subplot
 plt.subplot(2, 1, 1)
 
 for idx in range(len(m_V)):
@@ -30,6 +37,7 @@ plt.ylabel("Theta [deg]")
 plt.legend()
 plt.grid()
 
+# Plot controller output for each mass value
 plt.subplot(2, 1, 2)
 
 for idx in range(len(m_V)):
@@ -40,4 +48,5 @@ plt.ylabel("Tau [Nm]")
 plt.legend()
 plt.grid()
 
+# Show plots
 plt.show()
